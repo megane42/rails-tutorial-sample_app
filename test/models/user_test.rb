4951f4508decc78594c59a0cfcd4a58d5660e014
megane42 <h.kzm.gm@gmail.com>
2018-06-11 10:17:32 +0000

    add validations for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,4 +18,28 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "   "
     assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn).each do |valid_address|
+    test "email (#{valid_address}) should be accepted as valid format" do
+      @user.email = valid_address
+      assert @user.valid?
+    end
+  end
+
+  %w(user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com).each do |invalid_address|
+    test "email (#{invalid_address}) should be rejected as invalid format" do
+      @user.email = invalid_address
+      assert_not @user.valid?
+    end
+  end
 end
