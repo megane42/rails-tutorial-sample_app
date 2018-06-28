6c310cf8f37397ed574742bf986a12294fb2a8fb
megane42 <h.kzm.gm@gmail.com>
2018-06-28 20:05:21 +0900

    add remember_digest column and remember method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   end
 
   has_secure_password
+  attr_accessor :remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -21,8 +22,18 @@ class User < ApplicationRecord
             presence: true,
             length: { minimum: 6 }
 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(self.remember_token))
+  end
+
   def self.digest(str)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(str, cost: cost)
   end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
