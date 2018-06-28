31e39c6bb25906b118aaad1c83280b96f2a63eb1
megane42 <h.kzm.gm@gmail.com>
2018-06-28 17:35:01 +0900

    add fixture and successfull-login test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+
     if (user && user.authenticate(params[:session][:password]))
       log_in user
       redirect_to user_url(user)
     else
-      # error
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,25 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = users(:michael)
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+
+    assert_redirected_to user_path(@user)
+
+    follow_redirect!
+
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
   test "login with invalid information" do
     get login_path
     assert_template "sessions/new"
@@ -9,10 +28,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params: { session: { email: "", password: "" } }
 
     assert_template "sessions/new"
-    assert_select "form[action=\"#{login_path}\"][method=post]"
+    assert_select "form[action=?][method=post]", login_path
     assert_select "div.alert-danger"
 
     get root_path
-    assert_select "div.alert-danger", false
+    assert_select "div.alert-danger", count: 0
   end
 end
