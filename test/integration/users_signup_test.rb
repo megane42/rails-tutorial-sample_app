667feb23bd91805139c4a6e6956cbde4e59ed6c0
megane42 <h.kzm.gm@gmail.com>
2018-06-28 18:30:21 +0900

    login after user registration

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,6 +20,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template "users/show"
     assert_select "div.alert-success"
+    assert is_logged_in?
   end
 
 
