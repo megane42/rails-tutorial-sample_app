80d4c27383c50b6f0bae19f83499c659fdf29dba
megane42 <h.kzm.gm@gmail.com>
2018-07-01 00:24:36 +0900

    add image upload

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,25 +10,36 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
+
+    # invalid case
 
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
 
+    # valid case
+
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/files/rails.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
+
+    # delete post
 
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
+
+    # others can not delete
 
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
